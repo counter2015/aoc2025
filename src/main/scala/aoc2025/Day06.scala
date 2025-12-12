6d907ae1ee3b52730bbe671d9e42e0c78c55d062
counter2015 <voidcounter@gmail.com>
2025-12-12 10:23:42 +0800

    update day06 solution

---
+++ src/main/scala/aoc2025/Day06.scala
@@ -0,0 +1,55 @@
+package aoc2025
+
+import scala.collection.mutable.ArrayBuffer
+
+// problem link: https://adventofcode.com/2025/day/6
+object Day06 {
+  case class Problem(nums: List[Int], op: Char) {
+    def solution: BigInt = {
+      nums.map(BigInt(_)).reduceLeft((a, b) => op match {
+        case '*' => a * b
+        case '+' => a + b
+        case _   => throw new IllegalArgumentException(s"Invalid operator: $op")
+      })
+    }
+  }
+
+  object Problem {
+    def fromRaw(raw: Array[String]): Problem = {
+      val op = raw.last.head
+      val nums = raw.init.map(_.trim.toInt).toList
+      Problem(nums, op)
+    }
+  }
+  
+  def parseInput(input: String): List[Problem] = {
+    val lines = input.trim.split("\n")
+    val rawData = lines.map(_.trim.split("\\s+")).transpose
+    rawData.map(Problem.fromRaw).toList
+  }
+
+  def parseInput2(input: String): List[Problem] = {
+    val rawData = input.trim.split("\n").map(_.toCharArray).transpose.reverse
+    val nums = ArrayBuffer.empty[Int]
+    val problems = ArrayBuffer.empty[Problem]
+
+    rawData.map(_.mkString.trim).filter(_.nonEmpty).foreach(token => {
+      if (token.nonEmpty && (token.endsWith("+") || token.endsWith("*"))) {
+        val num = token.init.trim
+        val op = token.last
+        nums += num.toInt
+        problems += Problem(nums.toList, op)
+        nums.clear()
+      } else {
+        nums += token.trim.toInt
+      }})
+
+    problems.toList
+  }
+
+  def part1(input: String): BigInt = 
+    parseInput(input).map(_.solution).sum
+
+  def part2(input: String): BigInt = 
+    parseInput2(input).map(_.solution).sum
+}
---
+++ src/test/scala/aoc2025/Day06Suite.scala
@@ -0,0 +1,34 @@
+package aoc2025
+
+import Day06.*
+
+class Day06Suite extends munit.FunSuite {
+
+  val exampleInput = """
+                       |123 328  51 64 
+                       | 45 64  387 23 
+                       |  6 98  215 314
+                       |*   +   *   +""".stripMargin
+
+  test("example test that succeeds") {
+    val result = part1(exampleInput)
+    assertEquals(result, BigInt(4277556))
+  }
+
+  test("part1 input should succeeds") {
+    val input = TestUtils.readResource("day06.input")
+    val result = part1(input)
+    assertEquals(result, BigInt(6169101504608L))
+  }
+
+  test("example test for part2 should succeeds") {
+    val result = part2(exampleInput)
+    assertEquals(result, BigInt(3263827))
+  }
+
+  test("part2 input should succeeds") {
+    val input = TestUtils.readResource("day06.input")
+    val result = part2(input)
+    assertEquals(result, BigInt(10442199710797L))
+  }
+}
